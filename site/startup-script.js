5d1f932bb62be532fb8495f81a2a17f63dac56c6
davidlewis <djmlewis@hotmail.com>
2016-04-20 21:37:18 +0100

    list

--- site/startup-script.js
+++ site/startup-script.js
@@ -2,7 +2,8 @@
  * Created by davidjmlewis on 14/04/2016.
  */
 /* CONSTANTS */
-var kPhaseDrugsDiv_Stem = 'list_drugs_phase_';
+var kPhaseDiv_Stem = 'ph_';
+var kPhaseDrugsDiv_Stem = '_dr_';
 
 /* GLOBALS */
 var gCurrentWeight;
@@ -73,18 +74,6 @@ var gIndicationsList =
                             d_max: 300, d_units: "mg"
                         },
                         {
-                            d_name: "Rifampicin", d_how: "mg/Kg",
-                            d_mgkg_initial: 10, d_mgkg_min: 8, d_mgkg_max: 12,
-                            d_round_val: 150, d_round_direct: 1,
-                            d_max: 600, d_units: "mg"
-                        },
-                        {
-                            d_name: "Pyrazinamide", d_how: "mg/Kg",
-                            d_mgkg_initial: 25, d_mgkg_min: 20, d_mgkg_max: 30,
-                            d_round_val: 100, d_round_direct: 1,
-                            d_max: 2000, d_units: "mg"
-                        },
-                        {
                             d_name: "Ethambutol", d_how: "mg/Kg",
                             d_mgkg_initial: 15, d_mgkg_min: 15, d_mgkg_max: 20,
                             d_round_val: 100, d_round_direct: -1,
@@ -95,12 +84,6 @@ var gIndicationsList =
                 {//Continuation drug list
                     phase_name: "Continuation Phase",
                     drug_list: [
-                        {
-                            d_name: "Isoniazid", d_how: "mg/Kg",
-                            d_mgkg_initial: 5, d_mgkg_min: 4, d_mgkg_max: 6,
-                            d_round_val: 50, d_round_direct: 1,
-                            d_max: 300, d_units: "mg"
-                        },
                         {
                             d_name: "Rifampicin", d_how: "mg/Kg",
                             d_mgkg_initial: 10, d_mgkg_min: 8, d_mgkg_max: 12,
@@ -114,8 +97,59 @@ var gIndicationsList =
 
     ];
 
+/* GLOBAL STARTUPS */
+jQuery(document).one( "pagecreate", "#page_1", setupPageForIndication);
 
-jQuery(document).on( "pagecreate", "#page_1", setupPageForIndication);
+
+function buildSelectMenuWeight()
+{
+    var jqo_select_weight = $("#select_weight");
+    jqo_select_weight.on("change", buildDrugsLists);
+    for (var i = 30; i < 101; i++) {
+        //var opt =
+        $(document.createElement("option"))
+            .prop('value', i)
+            .prop('selected', i == 60)
+            .text(i.toString())
+            .appendTo(jqo_select_weight);
+    }
+    //refresh the selectmenu as created already in markup
+    jqo_select_weight.selectmenu('refresh');
+}
+
+function phaseButtonClicked()
+{
+    gCurrentIndicationIndex = ($(this).val());
+    buildDrugsLists();
+}
+
+function buildIndicationsRadio() {
+    var jqo_radiofieldsetcontainer = $('#fieldset_indications').controlgroup("container");//.find('.ui-controlgroup-controls');
+
+    // build radio button list gIndicationsList.length
+    for (var i = 0; i < gIndicationsList.length; i++) {
+        var id = gIndicationsList[i].d_indication_name;
+        var label = gIndicationsList[i].d_indication_name;
+
+        //var rad =
+        $(document.createElement("input"))
+            .attr('id', id)
+            .attr('type', 'radio')
+            .attr('name', 'indications')
+            .attr('value', i)
+            .prop('checked', i == gCurrentIndicationIndex)
+            .bind( "click", phaseButtonClicked)
+            .appendTo(jqo_radiofieldsetcontainer);
+
+        //var lab =
+        $(document.createElement("label"))
+            .attr('for', id)
+            .text(label)
+            .appendTo(jqo_radiofieldsetcontainer);
+    }
+    // activate control group
+    jqo_radiofieldsetcontainer.trigger("create");
+}
 
 function drugDoseString(drugInfo)
 {
@@ -192,118 +226,78 @@ function addInfoToCollapsible(arrayToParse, collapsibleDiv)
 
 }
 
-function rebuildDrugsLists()
+function uniqueIDforDrugInPhase(phaseIndex, drugIndex)
+{
+    return kPhaseDiv_Stem+phaseIndex+kPhaseDrugsDiv_Stem+drugIndex;
+}
+
+function buildDrugsLists()
 {
     var numDrugs;
-    var jqo_select_weight = $("#select_weight");
-    gCurrentWeight = jqo_select_weight.val();
+    // var jqo_select_weight = $("#select_weight");
+    gCurrentWeight = $("#select_weight").val();
+
+    var jqo_fieldcontain_drugs = $("#ui-field-contain-drugs");
+    jqo_fieldcontain_drugs.empty();
+
+    //add collapsible set for phases to hang on to
+    var phasesCollSet = $(document.createElement("div"))
+        .attr('data-role','collapsible-set')
+        .attr('data-collapsed-icon','false')
+        .attr('data-expanded-icon','false');
+
+
     //cycle thru the phases
     var numPhases = gIndicationsList[gCurrentIndicationIndex].phases.length;
     for (var ph=0;ph<numPhases;ph++)
     {
-        var phaseDivName = "#"+kPhaseDrugsDiv_Stem+ph.toString();
-        var jqo_CollapsibleSetForPhase = $(phaseDivName);
-        jqo_CollapsibleSetForPhase.empty();
+        //add a collapsible to hang the next set on
+        var phaseCollapsibleFor_PhaseDrugsCollSet = $(document.createElement("div"))
+            .attr('data-role','collapsible')
+            .attr('data-collapsed', (ph==0 ? "false" : "true"))
+            .append($(document.createElement("h3")).text(gIndicationsList[gCurrentIndicationIndex].phases[ph].phase_name));
+
+        //add collapsible set for drugs in the phase
+        var phaseDrugsCollSet = $(document.createElement("div"))
+            .attr('data-role','collapsible-set')
+            .attr('data-collapsed-icon','false')
+            .attr('data-expanded-icon','false');
+
 
         numDrugs = gIndicationsList[gCurrentIndicationIndex].phases[ph].drug_list.length;
         for(var drug=0; drug<numDrugs; drug++)
         {
+            var aDrugDiv = $(document.createElement("div"))
+                .attr('data-role','collapsible')
+                .attr('id',uniqueIDforDrugInPhase(ph,drug));
+
             var drugsInstructionsWarningsInfos = drugDoseString(gIndicationsList[gCurrentIndicationIndex].phases[ph].drug_list[drug]);
-            var aDiv = $(document.createElement("div"));
-            aDiv.attr('data-role','collapsible');
-            aDiv.append($(document.createElement("h3")).text(drugsInstructionsWarningsInfos.instructionsString));
-            addAlertsToCollapsible(drugsInstructionsWarningsInfos.warningArray,aDiv);
-            addInfoToCollapsible(drugsInstructionsWarningsInfos.infoArray,aDiv);
-            aDiv.appendTo(jqo_CollapsibleSetForPhase);
+            aDrugDiv.append($(document.createElement("h3")).text(drugsInstructionsWarningsInfos.instructionsString));
+            addAlertsToCollapsible(drugsInstructionsWarningsInfos.warningArray,aDrugDiv);
+            addInfoToCollapsible(drugsInstructionsWarningsInfos.infoArray,aDrugDiv);
+            aDrugDiv.appendTo(phaseDrugsCollSet);
+
         }
-        //activate the collapsible
-        jqo_CollapsibleSetForPhase.trigger("create");
+        phaseDrugsCollSet.appendTo(phaseCollapsibleFor_PhaseDrugsCollSet);
+        phaseCollapsibleFor_PhaseDrugsCollSet.appendTo(phasesCollSet);
+
     }
-}
 
-function buildSelectMenuWeight() 
-{
-    var jqo_select_weight = $("#select_weight");
-    jqo_select_weight.on("change", rebuildDrugsLists);
-    for (var i = 30; i < 101; i++) {
-        //var opt = 
-        $(document.createElement("option"))
-            .prop('value', i)
-            .prop('selected', i == 60)
-            .text(i.toString())
-            .appendTo(jqo_select_weight);
-    }
-    //refresh the selectmenu as created already in markup
-    jqo_select_weight.selectmenu('refresh');
-}
+    phasesCollSet.appendTo(jqo_fieldcontain_drugs);
 
-function buildIndicationsRadio() {
-    var jqo_radiofieldsetcontainer = $('#fieldset_indications').controlgroup("container");//.find('.ui-controlgroup-controls');
-
-    // build radio button list gIndicationsList.length
-    for (var i = 0; i < gIndicationsList.length; i++) {
-        var id = gIndicationsList[i].d_indication_name;
-        var label = gIndicationsList[i].d_indication_name;
-
-        //var rad = 
-        $(document.createElement("input"))
-            .attr('id', id)
-            .attr('type', 'radio')
-            .attr('name', 'indications')
-            .attr('value', i)
-            .prop('checked', i == gCurrentIndicationIndex)
-            .appendTo(jqo_radiofieldsetcontainer);
-    
-        //var lab = 
-        $(document.createElement("label"))
-            .attr('for', id)
-            .text(label)
-            .appendTo(jqo_radiofieldsetcontainer);
-    }
-    // activate control group
-    jqo_radiofieldsetcontainer.trigger("create");
-}
-
-function buildDrugsListScaffold()
-{
-    var jqo_fieldcontain_drugs = $("#ui-field-contain-drugs");
-    jqo_fieldcontain_drugs.empty();
-    /*add indication name
-    $(document.createElement("h3"))
-        .text(gIndicationsList[gCurrentIndicationIndex].d_indication_name)
-        .addClass("ui-bar ui-bar-a ui-corner-all")
-        .appendTo(jqo_fieldcontain_drugs);
-    */
-    // for each phase add the phase name and a collapsible set hanger
-    for (var ph=0;ph<gIndicationsList[gCurrentIndicationIndex].phases.length;ph++)
-    {
-        $(document.createElement("p"))
-            .text(gIndicationsList[gCurrentIndicationIndex].phases[ph].phase_name)
-            .addClass("ui-bar ui-bar-a ui-corner-all")
-            .appendTo(jqo_fieldcontain_drugs);
-        var phaseDivName = kPhaseDrugsDiv_Stem+ph.toString();
-        var newColSet = $(document.createElement("div"))
-            .attr('id',phaseDivName)
-            .attr('data-role','collapsible-set')
-            .attr('data-collapsed-icon','false')
-            .attr('data-expanded-icon','false');
-        newColSet.appendTo(jqo_fieldcontain_drugs);
-    }
     //activate the collapsible set components
     jqo_fieldcontain_drugs.trigger("create");
-
 }
 
 function setupPageForIndication()
 {
-    var jqo_slider_weight = $("#slider_weight");
-    jqo_slider_weight.on("slidestop change",rebuildDrugsLists);
+
 
     buildSelectMenuWeight();
     buildIndicationsRadio();
-    buildDrugsListScaffold();
-    rebuildDrugsLists();
+    buildDrugsLists();
 
-    
+
+
 
 }
